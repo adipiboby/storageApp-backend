36d4a28d178cefba9d98e2256073821af2b66d54
Adipi Manojkumar <adipimanojkumar@gmail.com>
2026-02-18 16:15:40 +0530

    add patch method on cors

--- app.js
+++ app.js
@@ -22,13 +22,11 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: "https://storageapp-frontend-user.netlify.app",
+    origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   }),
 );
-
 
 app.use("/directory", checkAuth, directoryRoutes);
 app.use("/file", checkAuth, fileRoutes);
