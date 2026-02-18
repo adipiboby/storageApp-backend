6a3fb2e9c8ab71af6101db043ca26d92743d8675
Adipi Manojkumar <adipimanojkumar@gmail.com>
2026-02-18 16:35:23 +0530

    remove slash in cors url

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: "https://storageapp-frontend-user.netlify.app/",
+    origin: "https://storageapp-frontend-user.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   }),
