124d06f5cad371c1bbd79f1bcf94317720c1d058
Adipi Manojkumar <adipimanojkumar@gmail.com>
2026-03-16 17:23:04 +0530

    added response message to in github webhook

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import userRoutes from "./routes/userRoutes.js";
 import webhookRoutes from "./routes/webhookRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import checkAuth from "./middlewares/authMiddleware.js";
- import { spawn } from "child_process";
+import { spawn } from "child_process";
 import { connectDB } from "./config/db.js";
 
 await connectDB();
@@ -22,7 +22,7 @@ app.use(express.json());
 app.set("trust proxy", 1);
 app.use(
   cors({
-    origin:"https://storageapp.adipi.in",
+    origin: "https://storageapp.adipi.in",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   }),
@@ -34,27 +34,29 @@ app.use("/subscriptions", checkAuth, subscriptionRoutes);
 app.use("/webhook", webhookRoutes);
 app.use("/", userRoutes);
 app.use("/auth", authRoutes);
-app.post("/github-webhook",(req,res)=>{
-const childprocess = spawn("bash", ["/home/ubuntu/deploy-frontend.sh"]);
+app.post("/github-webhook", (req, res) => {
+  const childprocess = spawn("bash", ["/home/ubuntu/deploy-frontend.sh"]);
 
-childprocess.stdout.on("data",(data)=>{
+  childprocess.stdout.on("data", (data) => {
     process.stdout.write(data);
-})
-childprocess.stderr.on("data",(err)=>{
-  process.stderr.write(err)
-})
-childprocess.on("close",(code)=>{
-    if(code===0){
-        console.log("scripted executed succesfully!")
-    }else{
-        console.log("script failed");
+  });
+  childprocess.stderr.on("data", (err) => {
+    process.stderr.write(err);
+  });
+  childprocess.on("close", (code) => {
+    res.json({ message: "ok" });
+    if (code === 0) {
+      console.log("scripted executed succesfully!");
+    } else {
+      console.log("script failed");
     }
-})
+  });
 
-childprocess.on("error",(err)=>{
-    console.log("error in spwaning the process!")
-})
-})
+  childprocess.on("error", (err) => {
+    res.json({ message: "ok" });
+    console.log("error in spwaning the process!");
+  });
+});
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500).json({ error: "Something went wrong!" });
